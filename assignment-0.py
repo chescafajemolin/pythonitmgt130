646867f5342f72cc30f6527406a55ded311d4b92
chescafajemolin <104746868+chescafajemolin@users.noreply.github.com>
2022-05-29 14:46:59 +0800

    Add files via upload

---
+++ assignment-0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+z = int(input("Enter third number:"))
+
+avg = float((x+y+z)/3)
+
+print(avg)
+
